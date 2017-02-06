9955afea395e8481b8f82d1bd5e7c776ab2cdfff
SauronSauron <sauren.thomas@gmail.com>
2017-02-06 15:03:36 +0100

    gitexercise

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String apa = "vem vet var du bor?";
+        System.out.println(apa);
+    }
+}
