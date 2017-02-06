a641fc55716cddc1a6f54e231debdf623c3682d0
kvissberg <max.kvissberg@gmail.com>
2017-02-06 15:15:09 +0100

    Max har lagt till en kommentar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //här är Max arbete på detta fantastiska projekt
+
         String apa = "vem vet var du bor?";
         System.out.println(apa);
     }
