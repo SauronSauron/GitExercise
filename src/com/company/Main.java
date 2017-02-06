8a3cec8bdf67cc03046bdb1b1f5e5039243611c1
SauronSauron <sauren.thomas@gmail.com>
2017-02-06 15:16:56 +0100

    class Food added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,10 @@ public class Main {
 
         String apa = "vem vet var du bor?";
         System.out.println(apa);
+
+        Food apMat = new Food();
+        apMat.setFood("Bananer");
+
+        System.out.println(apMat.getFood());
     }
 }
